79361ee56615adf87190e5819ec930c0d649bf2a
Brandon <brandonbsuen@gmail.com>
2017-12-09 23:37:57 -0800

    Hard Source Webpack Plugin implemented

--- webpack.common.js
+++ webpack.common.js
@@ -4,6 +4,7 @@ const webpack = require('webpack');
 const BabelMinifyPlugin = require('babel-minify-webpack-plugin');
 //const CommonShakePlugin = require('webpack-common-shake').Plugin;
 const DuplicatePackageCheckerPlugin = require('duplicate-package-checker-webpack-plugin');
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const OptimizeJsPlugin = require('optimize-js-plugin');
 const ResourceHintsWebpackPlugin = require('resource-hints-webpack-plugin');
@@ -39,6 +40,7 @@ module.exports = {
     }),
     //new CommonShakePlugin(),
     new DuplicatePackageCheckerPlugin(),
+    new HardSourceWebpackPlugin(),
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: './index.pug'
