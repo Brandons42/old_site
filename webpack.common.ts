393131508e81d5a87cfc75504cecc7b208817897
Brandon <brandonbsuen@gmail.com>
2018-01-01 00:05:06 -0800

    Favicons Webpack plugin set up

--- webpack.common.ts
+++ webpack.common.ts
@@ -2,6 +2,7 @@ import * as path from 'path';
 import * as webpack from 'webpack';
 
 import * as DuplicatePackageCheckerPlugin from 'duplicate-package-checker-webpack-plugin';
+import * as FaviconsWebpackPlugin from 'favicons-webpack-plugin';
 import * as HardSourceWebpackPlugin from 'hard-source-webpack-plugin';
 import * as HtmlWebpackPlugin from 'html-webpack-plugin';
 import * as OptimizeJsPlugin from 'optimize-js-plugin';
@@ -31,6 +32,7 @@ module.exports = {
   },
   plugins: [
     new DuplicatePackageCheckerPlugin(),
+    new FaviconsWebpackPlugin('./img/temporary-favicon.png'),
     new HardSourceWebpackPlugin(),
     new HtmlWebpackPlugin({
       filename: 'index.html',
